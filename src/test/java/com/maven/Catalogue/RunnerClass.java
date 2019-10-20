857626874929a65bf17372ddc6d3c0dd3b9fec96
HarithaMaganti <harithamaganti@gmail.com>
2019-10-20 17:46:39 +0100

    modified src

--- src/test/java/com/maven/Catalogue/RunnerClass.java
+++ src/test/java/com/maven/Catalogue/RunnerClass.java
@@ -6,11 +6,12 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources",dryRun = false,strict = true,tags = "@Register")
+@CucumberOptions(features = "src/test/resources",dryRun = false,strict = true,tags = "@Register",
+        plugin = {"json:src/test/cathJSON.json", "html:src/test/reports/cucumberReport.html"})
 //@RunWith(Cucumber.class)
 //@CucumberOptions(features = "src/test",
 // tags = {"@endtoend"},
-//plugin = {"json:src/test/cathJSON.json", "html:src/test/reports/cucumberReport.html"})
+
 
 public class RunnerClass {
 }
